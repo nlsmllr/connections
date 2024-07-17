8d8947ebae79647784e766d49a1dce2f0d6003b7
NMUProtofy <nils.mueller@protofy.com>
2024-07-17 21:50:31 +0200

    Added: Lives

--- src/app/Components/WordCardGrid.tsx
+++ src/app/Components/WordCardGrid.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState, useEffect } from 'react';
 import WordCard from './WordCard';
 
@@ -14,7 +13,7 @@ function shuffleArray(array: string[]): string[] {
 
 interface WordCardGridProps {
   wordGroups: string[][];
-  onNewWordGroups: () => void; // Add a callback prop to get new word groups
+  onNewWordGroups: () => void;
 }
 
 const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups }) => {
@@ -24,8 +23,8 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
   const [showResult, setShowResult] = useState<boolean>(false);
   const [showYay, setShowYay] = useState<boolean>(false);
   const [correctGroups, setCorrectGroups] = useState<number>(0);
-  const [lives, setLives] = useState(3);
-
+  const [lives, setLives] = useState(1);
+  const [showGameOver, setShowGameOver] = useState(false);
 
   useEffect(() => {
     const allWords = wordGroups.flat();
@@ -45,7 +44,6 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
       selectedWords.every(word => group.includes(word))
     );
     setResult(isSameGroup ? 'Correct!' : 'Incorrect!');
-    setShowResult(true);
 
     if (isSameGroup) {
       setShuffledWords(prevWords => prevWords.filter(word => !selectedWords.includes(word)));
@@ -57,18 +55,26 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
           setShowYay(true);
           setTimeout(() => {
             setShowYay(false);
-            setCorrectGroups(0); // Reset correct groups count
-            onNewWordGroups(); // Get new word groups
-          }, 1000); // Show "Yay!" for 1 second
+            setCorrectGroups(0);
+            onNewWordGroups();
+          }, 1000);
         }
-        setSelectedWords([]); // Clear selected words
-      }, 700); // Hide the result after 0.7 seconds
+        setSelectedWords([]);
+      }, 700);
     } else {
-      setLives(lives => lives - 1); // Decrement lives on incorrect answer
+      setLives(lives => {
+        if (lives - 1 === 0) {
+          setShowResult(false); // Hide result immediately if last life is lost
+          setTimeout(() => setShowGameOver(true), 0); // Delay the game over overlay
+        }
+        return lives - 1;
+      });
       setTimeout(() => {
-        setShowResult(false);
-        setSelectedWords([]); // Clear selected words after showing the result
-      }, 700); // Hide the result after 0.7 seconds
+        if (!showGameOver) {
+          setShowResult(true);
+        }
+        setSelectedWords([]);
+      }, 700);
     }
   };
 
@@ -87,10 +93,11 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
       <div className="flex justify-center mt-4">
         <button
           onClick={checkWords}
-          className={`px-2 py-1 md:mt-0 mt-12 sm:px-4 sm:py-2 text-black rounded ${selectedWords.length === 4
+          className={`px-2 py-1 md:mt-0 mt-12 sm:px-4 sm:py-2 text-black rounded ${
+            selectedWords.length === 4
               ? 'bg-emerald-800 transition-colors duration-500 md:hover:bg-amber-500 font-semibold uppercase cursor-pointer'
               : 'bg-red-500 uppercase font-semibold cursor-not-allowed'
-            }`}
+          }`}
           disabled={selectedWords.length !== 4}
         >
           Check Words
@@ -101,7 +108,13 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
           <span key={i} className="h-3 w-3 bg-blue-500 rounded-full mx-1"></span>
         ))}
       </div>
-      {showResult && (
+      {showGameOver && (
+        <div className="fixed inset-0 bg-black bg-opacity-85 flex items-center flex-col justify-center z-50">
+          <div className="text-white text-5xl font-semibold uppercase md:text-9xl">Game Over</div>
+          <div className="text-white text-xl font-semibold uppercase md:text-5xl">Try again later</div>
+        </div>
+      )}
+      {showResult && !showGameOver && (
         <div
           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
           style={{
@@ -125,6 +138,6 @@ const WordCardGrid: React.FC<WordCardGridProps> = ({ wordGroups, onNewWordGroups
       )}
     </div>
   );
-}
+};
 
 export default WordCardGrid;
