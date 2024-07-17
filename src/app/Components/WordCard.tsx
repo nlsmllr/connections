7102bf887e36b8c6145135925ffd9fd43ee90845
NMUProtofy <nils.mueller@protofy.com>
2024-07-17 18:18:03 +0200

    styling

--- src/app/Components/WordCard.tsx
+++ src/app/Components/WordCard.tsx
@@ -10,9 +10,9 @@ const WordCard: React.FC<WordCardProps> = ({ word, isSelected, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className={`flex items-center justify-center md:text-[16px] text-[11px] h-[90px] w-[90px] sm:h-24 sm:w-24 md:h-32 md:w-32 bg-zinc-500 text-black font-bold rounded shadow-md transition-colors duration-500 md:hover:bg-amber-500 cursor-pointer ${
-        isSelected ? 'bg-emerald-800' : ''
-      }`}
+      className={`flex items-center justify-center md:text-[16px] text-[11px] h-[90px] w-[90px] sm:h-24 sm:w-24 md:h-32 md:w-32 text-black font-bold rounded shadow-md cursor-pointer ${
+        isSelected ? 'bg-emerald-800' : 'bg-zinc-500 hover:bg-amber-500'
+      } transition-colors duration-500`}
     >
       {word}
     </div>
